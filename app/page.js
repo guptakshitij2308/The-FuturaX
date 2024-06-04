300ad8e107e18215f6565fed8e4ac275db02cee0
Kshitij <guptakshitij2308@gmail.com>
2024-06-04 14:18:08 +0530

    Added navigation and global layout for the next app.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
+import Link from "next/link.js";
+import Navigation from "./components/Navigation.js";
+
 export default function Page() {
-  return <h1>The FuturaX Inn. Welcome to the enclave.</h1>;
+  return (
+    <div>
+      {/* <Navigation /> */}
+      <h1>The FuturaX Inn. Welcome to the enclave.</h1>
+      <Link href="/cabins">Expore luxury cabins</Link>
+    </div>
+  );
 }
+
+// Optimization techniques : Prefetch all the routes linked to a certain page. Each page is downoloaded separately as a separate chunk and each page is cached for a certain amount of time
