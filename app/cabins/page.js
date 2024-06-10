fdbdc388e55631dbbddd35f988dfd69cf7f42690
Kshitij <guptakshitij2308@gmail.com>
2024-06-10 14:17:37 +0530

    Added layouts of different pages and data service for apis. Also added some base components.

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,32 @@
-// import Counter from "../_components/Counter.js";
+import CabinCard from "@/app/_components/CabinCard.js";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export default function Page() {
+  const cabins = [];
+
   return (
     <div>
-      <h1>Cabins page</h1>
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Modern Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&#39;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+      {cabins.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
